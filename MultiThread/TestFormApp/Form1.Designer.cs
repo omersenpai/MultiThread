c46c916cbd010ede9d850566be28669d077b6e79
ÖmerSargin <96244199+omersenpai@users.noreply.github.com>
2023-07-13 21:31:24 +0300

    İlk commitim

---
+++ MultiThread/TestFormApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TestFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCounter = new System.Windows.Forms.Button();
+            this.textBoxCounter = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(51, 55);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFile.TabIndex = 0;
+            this.btnReadFile.Text = "Dosya Oku";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(51, 113);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(173, 155);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCounter
+            // 
+            this.btnCounter.Location = new System.Drawing.Point(269, 55);
+            this.btnCounter.Name = "btnCounter";
+            this.btnCounter.Size = new System.Drawing.Size(75, 23);
+            this.btnCounter.TabIndex = 2;
+            this.btnCounter.Text = "Sayaç arttır";
+            this.btnCounter.UseVisualStyleBackColor = true;
+            this.btnCounter.Click += new System.EventHandler(this.btnCounter_Click);
+            // 
+            // textBoxCounter
+            // 
+            this.textBoxCounter.Location = new System.Drawing.Point(269, 135);
+            this.textBoxCounter.Name = "textBoxCounter";
+            this.textBoxCounter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCounter.TabIndex = 3;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(524, 87);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(132, 171);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBoxCounter);
+            this.Controls.Add(this.btnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnReadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCounter;
+        private System.Windows.Forms.TextBox textBoxCounter;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ MultiThread/TestFormApp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFormApp
+{
+    public partial class Form1 : Form
+    {
+        public int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReadFile_Click(object sender, EventArgs e)
+        {
+            string data = String.Empty;
+
+            Task<String> okuma = ReadFileAsync2();//ana threadi bloke etmesin diye thread kullanırız.
+            //ek thread kullanılmıyor
+
+            richTextBox2.Text = await new HttpClient().GetStringAsync("https://www.Google.com");//data text e yazsın öyle geç diye await koyduk.
+                                                                                                //ek thread kullanılmıyor.
+
+            data = await okuma;
+
+
+            richTextBox1.Text = data;
+        }
+
+        private void btnCounter_Click(object sender, EventArgs e)
+        {
+            textBoxCounter.Text = counter++.ToString();
+        }
+
+        private string ReadFile()
+        {
+            string data = string.Empty;
+            using (StreamReader s = new StreamReader("DENEME.txt"))
+            {//senkron kodun calısma sekli
+                Thread.Sleep(5000); //ana thread uyudugu icin başka şey yapamayız.
+                data = s.ReadToEnd();
+
+            }
+            return data;
+
+        }
+        //void  Task
+        //string Task<string>
+        private async Task<string> ReadFileAsync() //asenkron oldugu anlasılması icin.Dosya okundugu anda başka işlemler yapabiliriz.
+        {
+            string data = string.Empty;
+            using (StreamReader s = new StreamReader("DENEME.txt"))
+            {
+                Task<string> myTask = s.ReadToEndAsync();//bu metod string metod donecek.Buraada thread yok
+                                                         //10 sn sürdü.15 sn sürerse işlem altta 5 saniye daha bekler.
+                                                         //her zaman thread kullanılmaz.  
+                                                         //bu arada istenen işlemler yapılabilir.
+
+                await Task.Delay(5000);
+                data = await myTask;
+                return data;
+            }
+
+        }
+
+
+        private Task<string> ReadFileAsync2()
+        {
+            StreamReader s = new StreamReader("DENEME.txt"); //using blogunu kaldırırsak çalışır bunda
+            
+                return s.ReadToEndAsync();
+            
+        }
+
+
+
+        //ReadToEndAsync metodu StreamReader sınıfında bulunan bir metottur.Ve dosyanın tamamını asenkron bir şekilde okumak için kullanılır.
+        //bu metot, dosyanın okunması işlemini başlatır ve işlem tamamlandığında bir Task<string> nesnesi döndürür.
+        //await anahtar kelimesi, asenkron bir işlemin tamamlanmasını beklemek için kullanılır. Bir metot içinde await kullanıldığında, o noktada metotun yürütmesi durur ve işlem tamamlanana kadar bekler.
+        //Bir metotda await kullanmak istiyorsanız, o metodu async olarak işaretlemelisiniz.
+        //await yalnızca Task veya Task<T> döndüren metotlarla kullanılabilir
+        //Özetlemek gerekirse, ReadToEndAsync gibi asenkron bir metodu await ile beklemek, metot çağrısının tamamlanmasını beklerken diğer işlemlerin yapılmasını sağlar. Bu, kullanıcı arayüzünün donmamasını ve diğer işlemlerin devam etmesini sağlar. await anahtar kelimesi, asenkron işlemleri daha etkin bir şekilde yönetmenizi ve yanıt beklerken zaman kazanmanızı sağlar.
+    }
+}
+
